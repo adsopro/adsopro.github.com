10ea03551a8e184ef435d3453548cbebc623115d
Julian Velasco <adsoftwarepro02@gmail.com>
2023-04-21 19:56:53 -0500

    Cambio de carpeta

--- Boletines/javascript/main.js
+++ Boletines/javascript/main.js
@@ -19,7 +19,7 @@ links.forEach(function (link) {
     content.classList.add("animate__animated");
 
     setTimeout(function () {
-      location.href = "/";
+      location.href = "/BOLETINES";
     }, 600);
 
     return false;
