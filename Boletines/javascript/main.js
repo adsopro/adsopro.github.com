c1ea68aaab3c4f00577a075ecf8d07ebc4dd3dc8
Julian Velasco <adsoftwarepro02@gmail.com>
2023-04-21 19:53:37 -0500

    Agrego el proyecto Boletines

---
+++ Boletines/javascript/main.js
@@ -0,0 +1,32 @@
+/*let links = document.querySelectorAll("a");
+links.forEach(function (link) {
+  console.log(link);
+});*/
+
+let links = document.querySelectorAll(".close");
+links.forEach(function (link) {
+  //Agregar un evento click
+  link.addEventListener("click", function (ev) {
+    ev.preventDefault();
+    let content = document.querySelector(".content");
+
+    //Quitar las animaciones de la clase
+    content.classList.remove("animate__bounceInDown");
+    content.classList.remove("animate__animated");
+
+    //Agregar animaciones de salida
+    content.classList.add("animate__bounceOutDown");
+    content.classList.add("animate__animated");
+
+    setTimeout(function () {
+      location.href = "/";
+    }, 600);
+
+    return false;
+  });
+});
+
+/*let iconos = document.querySelectorAll("i");
+iconos.forEach(function (icono) {
+  icono.classList.remove("fa-star");
+});*/
